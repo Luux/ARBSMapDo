f11df3f12fc6c74df27d2cab8e5dd349675b9638
Luux <eric.schoelzel@yahoo.de>
2020-03-03 22:36:46 +0100

    + proper User Input validation in the assistant

--- arbsmapdo.py
+++ arbsmapdo.py
@@ -40,14 +40,14 @@ def init_config(args):
     return args_dict
 
 
-def handle_missing_arguments(config_dict):
+def missing_argument_assistant(config_dict):
     # Kinda ugly. But processing user input config stuff will always be.
 
     if config_dict.get("download_dir") is None:
         print("Download directory not yet specified. Please input the download directory (usually '[BeatSaberPath]\\Beat Saber_Data\\CustomLevels').")
         print("This will be saved to the preset if specified or to the default preset.")
-        download_dir = str(dir_script.joinpath(Path(input())).absolute())
-        config_dict["download_dir"] = download_dir
+        response = str(get_validated_input(Path).absolute())
+        config_dict["download_dir"] = response
 
         # write the path to the used preset if it's not set at all
         save_config(config_dict, default_config_name)
@@ -57,9 +57,8 @@ def handle_missing_arguments(config_dict):
         print("(A Level will be downloaded if there's at least one ranked difficulty)")
         print("0 - All Maps")
         print("1 - Ranked Only")
-        response = input()
-        ranked_only = "0" if response is "0" else "1"
-        config_dict["ranked_only"] = ranked_only
+        response = get_validated_input(int, default=1, choices=[0, 1])
+        config_dict["ranked_only"] = response
     
     if config_dict.get("scoresaber_sorting") is None:
             print("Which sorting (from scoresaber side) should be used? (Default: 1)")
@@ -67,42 +66,37 @@ def handle_missing_arguments(config_dict):
             print("1 - Date Ranked")
             print("2 - Scores Set")
             print("3 - Star Difficulty (only if ranked)")
-            response = input()
-            config_dict["scoresaber_sorting"] = response if response in ["0", "1", "2", "3"] else "1"
+            response = get_validated_input(dst_type=int, default=1, choices=[0, 1, 2, 3])
+            config_dict["scoresaber_sorting"] = response
 
     if config_dict.get("levels_to_download") is None:
         print("How many levels do you want to download?")
-        response = int(input())
+        response = get_validated_input(int, min_value=1)
         config_dict["levels_to_download"] = response
     
     if config_dict.get("stars_min") is None:
         print("Minimum Stars? (Default: 0)")
-        response = input()
-        response = float(response.replace(",", ".")) if response is not "" else 0
+        response = get_validated_input(float, default=0, min_value=0, max_value=50)
         config_dict["stars_min"] = response
 
     if config_dict.get("stars_max") is None:
         print("Maximum Stars? (Default: 50)")
-        response = input()
-        response = float(response.replace(",", ".")) if response is not "" else 50
+        response = get_validated_input(float, default=50, min_value=0, max_value=50)
         config_dict["stars_max"] = response
 
     if config_dict.get("vote_ratio_min") is None:
         print("What's the minimum percentage of upvotes (of total votes) the map should have? (Value between 0 and 1, Default: 0)")
-        response = input()
-        response = float(response.replace(",", ".")) if response is not "" else 0
+        response = get_validated_input(float, default=0, min_value=0, max_value=1)
         config_dict["vote_ratio_min"] = response
     
     if config_dict.get("duration_min") is None:
         print("Do you want to set a minimum song duration (in seconds)? (Default: 0)")
-        response = input()
-        response = float(response.replace(",", ".")) if response is not "" else 0
+        response = get_validated_input(float, default=0, min_value=0)
         config_dict["duration_min"] = response
     
     if config_dict.get("duration_max") is None:
         print("Do you want to set a maximum song duration (in seconds)? (Default: infinite)")
-        response = input()
-        response = float(response.replace(",", ".")) if response is not "" else float("inf")
+        response = get_validated_input(float, default=float("inf"), min_value=0)
         config_dict["duration_max"] = response
 
     # Non-common options that are only available via command line or presets
@@ -125,6 +119,41 @@ def handle_missing_arguments(config_dict):
         print("Preset saved: {}\n Saved to: {}".format(str(config_dict), str(save_config)))
 
     return config_dict
+
+
+def get_validated_input(dst_type, default=None, choices=None, min_value=None, max_value=None):
+    while True:
+        response = input()
+        if response is "" and default is not None:
+            return default
+        else:
+            try:
+                if dst_type is float:
+                    response = response.replace(",", ".")
+
+                elif dst_type is Path:
+                    response = dir_script.joinpath(Path(input()))
+
+                response = dst_type(response)
+
+                if dst_type is int or dst_type is float:
+                    if min_value is not None and response < min_value:
+                        print("Value has to be >= " + str(min_value))
+                        continue
+                    if max_value is not None and response > max_value:
+                        print("Value has to be <= " + str(max_value))
+                        continue
+
+                if choices is None:
+                    return response
+                elif response in choices:
+                    return response
+                else:
+                    print("Please choose one of the given options: " + str(choices))
+            except:
+                print("Please input a valid value.")
+                continue
+
 
 def save_config(config: dict, path: str):
     path = dir_script.joinpath(Path(path))
@@ -160,7 +189,7 @@ if __name__ == "__main__":
     print("All options are available directly from the command line as well as via presets.\n")
 
     config = init_config(args)
-    config = handle_missing_arguments(config)
+    config = missing_argument_assistant(config)
     downloader = advanced_downloader(config)
     downloader.start()
 
